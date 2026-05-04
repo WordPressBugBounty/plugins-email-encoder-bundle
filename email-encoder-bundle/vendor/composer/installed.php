ca7ac210c9a6eb4d04888b652421e5fa23b1f895
Sean Murphy <sean@iamseanmurphy.com>
2026-05-04 05:20:14 +0000

    2.5.0

--- email-encoder-bundle/vendor/composer/installed.php
+++ email-encoder-bundle/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'onlineoptimisation/email-encoder-bundle',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'c2fde7bda3765ee585e0e13a0d13269e3e573a40',
+        'reference' => '839f4fcf74a0600483847074394d3d890b540d6f',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'onlineoptimisation/email-encoder-bundle' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'c2fde7bda3765ee585e0e13a0d13269e3e573a40',
+            'reference' => '839f4fcf74a0600483847074394d3d890b540d6f',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
